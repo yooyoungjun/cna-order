c5f93b1128441fe2ee0562b8c4a808a8e68b45a8
yooyoungjun <iamakira83@gmail.com>
2020-08-20 16:50:51 +0900

    1st commit...

---
+++ src/main/java/shop/Order.java
@@ -0,0 +1,71 @@
+package shop;
+
+import javax.persistence.*;
+import org.springframework.beans.BeanUtils;
+import java.util.List;
+
+@Entity
+@Table(name="Order_table")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Long id;
+    private String productId;
+    private Integer qty;
+
+    @PostPersist
+    public void onPostPersist(){
+        Ordered ordered = new Ordered();
+        BeanUtils.copyProperties(this, ordered);
+        ordered.publishAfterCommit();
+
+
+    }
+
+    @PreRemove
+    public void onPreRemove(){
+        OrderCanceled orderCanceled = new OrderCanceled();
+        BeanUtils.copyProperties(this, orderCanceled);
+        orderCanceled.publishAfterCommit();
+
+        //Following code causes dependency to external APIs
+        // it is NOT A GOOD PRACTICE. instead, Event-Policy mapping is recommended.
+
+        shop.external.Cancellation cancellation = new shop.external.Cancellation();
+        // mappings goes here
+        cancellation.setOrderId(this.getId());
+        cancellation.setStatus("DeliveryCancelled");
+        OrderApplication.applicationContext.getBean(shop.external.CancellationService.class)
+            .cancel(cancellation);
+
+
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+    public Integer getQty() {
+        return qty;
+    }
+
+    public void setQty(Integer qty) {
+        this.qty = qty;
+    }
+
+
+
+
+}
---
+++ src/main/java/shop/OrderRepository.java
@@ -0,0 +1,8 @@
+package shop;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface OrderRepository extends PagingAndSortingRepository<Order, Long>{
+
+
+}
